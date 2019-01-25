9a3378ac1c7cc6b022946d30bca095ee8bf70fa4
Luke Helg <luke@exolever.com>
2019-01-25 09:59:12 +0900

    Add site meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   siteMetadata: {
-    title: 'gatsby-starter-typescript-plus',
-    description: 'A starter kit for TypeScript-based Gatsby projects with sensible defaults.',
-    siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com',
+    title: 'Exoleaders',
+    description: '',
+    siteUrl: '',
     author: {
-      name: 'Resi Respati',
-      url: 'https://twitter.com/resir014',
-      email: 'resir014@gmail.com'
+      name: '',
+      url: '',
+      email: ''
     }
   },
   plugins: [
@@ -47,7 +47,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com'
+        siteUrl: 'http://exodevhub.com'
       }
     },
     'gatsby-plugin-emotion',
