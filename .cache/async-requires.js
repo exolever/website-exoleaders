155164fd6d31818ab666019b42035d751c358c2f
Alex King <alexanderddking@gmail.com>
2019-01-29 11:46:34 +0900

    Added now.json for gastby

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,6 +3,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-templates-page-tsx": () => import("/Users/alexanderking/Projects/website-exoleaders/src/templates/page.tsx" /* webpackChunkName: "component---src-templates-page-tsx" */),
+  "component---cache-dev-404-page-js": () => import("/Users/alexanderking/Projects/website-exoleaders/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-tsx": () => import("/Users/alexanderking/Projects/website-exoleaders/src/pages/404.tsx" /* webpackChunkName: "component---src-pages-404-tsx" */),
   "component---src-pages-index-tsx": () => import("/Users/alexanderking/Projects/website-exoleaders/src/pages/index.tsx" /* webpackChunkName: "component---src-pages-index-tsx" */)
 }
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-templates-page-tsx": hot(preferDefault(require("/Users/alexanderking/Projects/website-exoleaders/src/templates/page.tsx"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/alexanderking/Projects/website-exoleaders/.cache/dev-404-page.js"))),
   "component---src-pages-404-tsx": hot(preferDefault(require("/Users/alexanderking/Projects/website-exoleaders/src/pages/404.tsx"))),
   "component---src-pages-index-tsx": hot(preferDefault(require("/Users/alexanderking/Projects/website-exoleaders/src/pages/index.tsx")))
 }
