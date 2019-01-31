a08b37139770158cde54de1b55d1e9e07ec6f244
Shinsuke Marty Yamada <shinsuke.yamada@exolever.com>
2019-01-31 13:42:13 +0900

    Remove cache files

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/alexanderking/Projects/website-exoleaders/node_modules/gatsby-plugin-google-analytics/gatsby-browser'),
-      options: {"plugins":[],"trackingId":"UA-133300815-1","head":true,"anonymize":true,"respectDNT":true,"exclude":["/preview/**","/do-not-track/me/too/"],"variationId":"0","cookieDomain":"exoleaders.com"},
-    },{
-      plugin: require('/Users/alexanderking/Projects/website-exoleaders/node_modules/gatsby-plugin-canonical-urls/gatsby-browser'),
-      options: {"plugins":[],"siteUrl":"http://exodevhub.com"},
-    }]
